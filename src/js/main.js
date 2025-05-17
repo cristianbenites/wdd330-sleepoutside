8ec2b8a9bf6957fcf8918f6df94a426d211b5ca9
cristianbenites <cristian.jbenites@gmail.com>
2025-05-17 19:28:43 -0400

    run lint and format

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,10 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
-const dataSource = new ProductData('tents');
+const dataSource = new ProductData("tents");
 const productList = new ProductList(
-  'bla',
+  "bla",
   dataSource,
-  document.querySelector('.product-list')
+  document.querySelector(".product-list"),
 );
 productList.init();
-
