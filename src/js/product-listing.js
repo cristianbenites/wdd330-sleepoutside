8174ec3ce3c3d29caeea7bd823acb6c9572e100a
cristianbenites <cristian.jbenites@gmail.com>
2025-05-23 23:17:43 -0400

    W03 Individual Activity: Expand Inventory

---
+++ src/js/product-listing.js
@@ -0,0 +1,17 @@
+import ProductData from './ProductData.mjs';
+import ProductList from './ProductList.mjs';
+import { getParam, loadHeaderFooter } from './utils.mjs';
+
+loadHeaderFooter();
+
+const category = getParam('category');
+const dataSource = new ProductData();
+const productList = new ProductList(
+  category,
+  dataSource,
+  document.querySelector('.product-list'),
+);
+productList.init();
+
+document.querySelector('.products h2').textContent = `Top Products: ${category}`;
+
