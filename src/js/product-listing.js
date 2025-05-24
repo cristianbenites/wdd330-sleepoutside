136960b45d6b9a444ddc74ebedee54f2320202e6
cristianbenites <cristian.jbenites@gmail.com>
2025-05-23 23:18:58 -0400

    run lint

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,17 +1,17 @@
-import ProductData from './ProductData.mjs';
-import ProductList from './ProductList.mjs';
-import { getParam, loadHeaderFooter } from './utils.mjs';
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
-const category = getParam('category');
+const category = getParam("category");
 const dataSource = new ProductData();
 const productList = new ProductList(
   category,
   dataSource,
-  document.querySelector('.product-list'),
+  document.querySelector(".product-list"),
 );
 productList.init();
 
-document.querySelector('.products h2').textContent = `Top Products: ${category}`;
-
+document.querySelector(".products h2").textContent =
+  `Top Products: ${category}`;
