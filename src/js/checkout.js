a49078380bcd8294d9a23e17b286fdcbceee39ee
Cristian <cristian.jbenites@gmail.com>
2025-09-23 22:53:01 -0400

    w04 team activity - checkout

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,3 +1,17 @@
+import CheckoutProcess from "./CheckoutProcess.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
+
+const order = new CheckoutProcess("so-cart", ".items-summary");
+order.init();
+
+document
+  .querySelector("#zip")
+  .addEventListener("blur", order.calculateOrderTotal.bind(order));
+
+document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  order.checkout();
+});
